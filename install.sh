065535f6d7ec84693fdda0891fa53613809f72ea
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2026-03-13 17:15:10 +0100

    Changed install.sh to get absolute poath

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # This script will create symlink for all the folder in this repository into their required location.
 
 # For more clarity, please clone this repository in ~/.config/
-#
+CURRENT_DIR=$(pwd)
 
-ln -s ./quickshell/ ~/.config/quickshell
-ln -s ./hypr/ ~/.config/hypr
+ln -s "$CURRENT_DIR"/quickshell/ ~/.config/quickshell
+ln -s "$CURRENT_DIR"/hypr/ ~/.config/hypr
+ln -s "$CURRENT_DIR"/kitty/ ~/.config/kitty
